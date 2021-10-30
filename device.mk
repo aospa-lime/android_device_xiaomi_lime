9d34b0b8222bb331981c0e0429f5cd9199def282
Gagan Malvi <malvigagan@gmail.com>
2021-10-30 19:37:44 +0200

    lime: Add support for Qualcomm common components.
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- device.mk
+++ device.mk
@@ -34,5 +34,26 @@ PRODUCT_PACKAGES += fastbootd
 # Add default implementation of fastboot HAL.
 PRODUCT_PACKAGES += android.hardware.fastboot@1.0-impl-mock
 
+# Board
+TARGET_BOARD_PLATFORM := bengal
+
+# QTI
+QTI_COMPONENTS := adreno
+QTI_COMPONENTS += audio
+QTI_COMPONENTS += av
+QTI_COMPONENTS += bt
+QTI_COMPONENTS += display
+QTI_COMPONENTS += gps
+QTI_COMPONENTS += init
+QTI_COMPONENTS += media-legacy
+QTI_COMPONENTS += overlay
+QTI_COMPONENTS += perf
+QTI_COMPONENTS += telephony
+QTI_COMPONENTS += usb
+QTI_COMPONENTS += vibrator
+QTI_COMPONENTS += wfd
+QTI_COMPONENTS += wlan
+TARGET_COMMON_QTI_COMPONENTS := $(QTI_COMPONENTS)
+
 # Default kernel revision
 TARGET_KERNEL_VERSION := 4.19

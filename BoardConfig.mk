2527fd76ea3b5bd0325cb2bf4dccac31c651edaf
Gagan Malvi <malvigagan@gmail.com>
2021-11-01 06:07:24 +0100

    lime: mlipay: Initial proprietary pull from MIUI 21.5.13
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -103,9 +103,7 @@ include hardware/qcom/gps/gps_vendor_board.mk
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml
-DEVICE_MANIFEST_FILE := \
-    $(DEVICE_PATH)/configs/vintf/manifest.xml \
-    $(DEVICE_PATH)/configs/vintf/xiaomi_manifest.xml
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 
 # Hacks

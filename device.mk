2990c1bd7eb82bbf4bdb7ccc3ad4db4a6d4f7e4f
Gagan Malvi <malvigagan@gmail.com>
2021-12-23 05:59:00 +0100

    lime: Support VNDK 30.
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- device.mk
+++ device.mk
@@ -192,6 +192,9 @@ $(call inherit-product, vendor/xiaomi/lime/lime-vendor.mk)
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
+# VNDK
+PRODUCT_EXTRA_VNDK_VERSIONS := 30
+
 # WLAN
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wlan/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini

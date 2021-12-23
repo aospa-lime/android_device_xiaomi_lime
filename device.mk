37c4129460e9509690fcd475d67bb197deb8f88d
Gagan Malvi <malvigagan@gmail.com>
2021-12-23 06:02:21 +0100

    lime: Update ClearKey service to 1.4.
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- device.mk
+++ device.mk
@@ -90,7 +90,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.4-service.clearkey
 
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)

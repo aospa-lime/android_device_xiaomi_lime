05fd4832c0cabe5f641bc0cd17daaabfd252da7f
Gagan Malvi <malvigagan@gmail.com>
2021-12-26 12:03:17 +0100

    lime: Add up Xiaomi changes to manifest.
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,10 +109,9 @@ TARGET_COPY_OUT_PRODUCT := product
 TARGET_SCREEN_DENSITY := 420
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml \
-    $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/xiaomi_manifest.xml
 DEVICE_MATRIX_FILE += $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 
 # Hacks

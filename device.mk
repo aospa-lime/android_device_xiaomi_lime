ec3d4579cb3b8611e3678e942dff5f09387b78a5
Gagan Malvi <malvigagan@gmail.com>
2022-01-16 16:54:24 +0100

    lime: Update proprietary stack from LA.UM.9.12.r1-13300-SMxx50.QSSI12.0.
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- device.mk
+++ device.mk
@@ -57,7 +57,7 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
-    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio@2.1 \
     com.dsi.ant@1.0.vendor \
     com.qualcomm.qti.bluetooth_audio@1.0.vendor \
     libbluetooth_audio_session \
